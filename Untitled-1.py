7a4051cfc20b5453973a96cb1f29f5328ef01ea3
pychansjtu <104835210+pychansjtu@users.noreply.github.com>
2022-05-07 21:37:57 +0800

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,5 +2,5 @@ def fib(n):
     if n ==0 or n == 1:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib(n-1) + fib(n-3) + fib(n-2)
 print([fib(n)for n in range(30)])
